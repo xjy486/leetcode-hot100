bddb628df7f9bd1a657356f80193b29871b796b3
云来云往 <486136834@qq.com>
2025-09-02 17:12:33 +0800

    环形链表 2025/09/02

---
+++ 25.环形链表.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
+ * }
+ */
+/*
+ * 快慢指针
+ * 快指针每次移动两步，慢指针每次移动一步，如果有环，快慢指针会相遇
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
