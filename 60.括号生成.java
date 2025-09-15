e5c01c2db7b6d235450e20e0de50dddc26bf3c46
云来云往 <486136834@qq.com>
2025-09-15 11:16:37 +0800

    括号生成 2025/09/15

---
+++ 60.括号生成.java
@@ -0,0 +1,49 @@
+class Solution {
+    List<String> result = new ArrayList<>();
+
+    public List<String> generateParenthesis(int n) {
+        backTrack(n, new StringBuilder(), 0, 0);
+        return result;
+    }
+
+    public void backTrack(int n, StringBuilder sb, int leftCount, int rightCount) {
+        if (sb.length() == 2 * n && isLegal(sb)) {
+            result.add(sb.toString());
+            return;
+        }
+        for (int i = 0; i < 2; i++) {
+            if (i == 0 && leftCount <= n) {
+                sb.append('(');
+                leftCount++;
+                backTrack(n, sb, leftCount, rightCount);
+                sb.deleteCharAt(sb.length() - 1);
+                leftCount--;
+            } else if (i == 1 && rightCount <= leftCount) {
+                sb.append(')');
+                rightCount++;
+                backTrack(n, sb, leftCount, rightCount);
+                sb.deleteCharAt(sb.length() - 1);
+                rightCount--;
+            }
+
+        }
+    }
+
+    public boolean isLegal(StringBuilder sb) {
+        List<Character> stack = new ArrayList<>();
+        for (int i = 0; i < sb.length(); i++) {
+            if (sb.charAt(i) == '(') {
+                stack.add(sb.charAt(i));
+            } else {
+                if (stack.size() != 0) {
+                    stack.remove(stack.size() - 1);
+                } else {
+                    return false;
+                }
+            }
+        }
+        if (stack.size() == 0)
+            return true;
+        return false;
+    }
+}
