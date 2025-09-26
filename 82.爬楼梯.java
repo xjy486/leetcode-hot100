999d25f2bf6a23e9b0ed95d496c9dc8b12fbdde0
云来云往 <486136834@qq.com>
2025-09-26 16:32:46 +0800

    爬楼梯 2025/09/26

--- 82.爬楼梯.java
+++ 82.爬楼梯.java
@@ -2,6 +2,13 @@ class Solution {
     public int climbStairs(int n) {
         if (n < 3)
             return n;
-        return climbStairs(n - 1) + climbStairs(n - 2);
+
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        for (int i = 3; i <= n; i++) {
+            list.add(list.get(list.size() - 1) + list.get(list.size() - 2));
+        }
+        return list.get(list.size() - 1);
     }
 }
