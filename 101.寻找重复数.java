aa87a16b2e6bc85e280630cbc6d6c3ce13fc0664
云来云往 <486136834@qq.com>
2025-10-13 16:16:34 +0800

    寻找重复的数 2025/10/13

---
+++ 101.寻找重复数.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int fast = nums[0];
+        int slow = nums[0];
+        do {
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+        } while (fast != slow);
+        slow = nums[0];
+        while (fast != slow) {
+            fast = nums[fast];
+            slow = nums[slow];
+        }
+        return slow;
+    }
+}
