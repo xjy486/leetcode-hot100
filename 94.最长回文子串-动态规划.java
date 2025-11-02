cac593e9fd202622eb8027e4bec529eec5846270
云来云往 <486136834@qq.com>
2025-11-02 08:56:53 +0800

    最长回文子串 2025/11/2

---
+++ 94.最长回文子串-动态规划.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+        int maxLen = 1;
+        int begin = 0;
+
+        boolean[][] dp = new boolean[len][len];
+        char[] charArray = s.toCharArray();
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+        for (int j = 1; j < len; j++) {
+            for (int i = 0; i < j; i++) {
+                if (charArray[i] != charArray[j]) {
+                    dp[i][j] = false;
+                } else {
+                    // 如果s[i][j]去掉头尾两个字符后长度小于2
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+
+                if (dp[i][j] == true && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+        return s.substring(begin, begin + maxLen);
+
+    }
+}
